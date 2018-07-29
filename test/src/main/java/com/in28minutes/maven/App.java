dec8e0d6e01f6c10537c83e74c185ac043661aa1
shubham0705 <41825270+shubham0705@users.noreply.github.com>
2018-07-29 15:25:29 +0530

    Update App.java

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World  Test Test! " );
+        System.out.println( "Hello World  Test Test! 2" );
     }
 
 	public int calculateSomething() {
